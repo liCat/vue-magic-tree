2547210f4b5f455deac4424944e55e5be8b1cfb6
keli <keli@wanda.cn>
2017-08-22 01:04:38 +0800

    magic tree

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,75 +1,130 @@
 <template>
   <div class="hello">
     <h2>Vue magic tree</h2>
-    <vue-magic-tree :tree="tree"></vue-magic-tree>
+    <vue-magic-tree
+      :tree="conditionsData"
+      :logic-operator-list="logicOperatorList"
+      :operator-list="operatorList"
+      :property-list='propertyList'
+    ></vue-magic-tree>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      tree:[
-        {
-          type: 'branch',
-          operationSign: "and",
-          leaf:[
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        propertyList: [
+          {
+            label: "pv",
+            value: "1",
+          },
+          {
+            label: "uv",
+            value: "2",
+          },
+          {
+            label: "啥子",
+            value: "3",
+          },
+        ],
+        operatorList: [
+          {
+            label: "大于",
+            value: "GT",
+          },
+          {
+            label: "等于",
+            value: "EQ",
+          },
+          {
+            label: "不等于",
+            value: "NEQ",
+          },
+        ],
+        logicOperatorList: [
+          {
+            label: "与",
+            value: "and",
+          },
+          {
+            label: "或",
+            value: "or",
+          }
+        ],
+        conditionsData: [{
+          conditions: [
             {
-              type: "leaf",
-              key: "a",
-              judgeSign: "大于",
-              value: 333
+              id: 1,
+              operator: "GT",
+              propertyCode: "1",
+              value: "true"
             },
             {
-              type: "leaf",
-              key: "b",
-              judgeSign: "小于",
-              value: 444
-            },
-            {
-              type: "leaf",
-              key: "c",
-              judgeSign: "等于",
-              value: 555
-            },
-            {
-              type: "branch",
-              operationSign: "or",
-              leaf: [
-                {
-                  type: "leaf",
-                  key: "d",
-                  judgeSign: "等于",
-                  value: 532
-                },
-              ]
+              id: 2,
+              operator: "GT",
+              propertyCode: "2",
+              value: "2017-01-01"
             }
-          ]
-        }
-      ]
+          ],
+          groups: [
+            {
+              conditions: [
+                {
+                  id: 3,
+                  operator: "EQ",
+                  propertyCode: "3",
+                  value: "2017-01-01 12:12:12"
+                }
+              ],
+              groups: [
+                {
+                  conditions: [
+                    {
+                      id: 4,
+                      operator: "NEQ",
+                      propertyCode: "4",
+                      value: "1.11"
+                    },
+                    {
+                      id: 24916,
+                      operator: "GT",
+                      propertyCode: "4",
+                      value: "333"
+                    }
+                  ],
+                  groups: [],
+                  logicalOperation: "and"
+                }
+              ],
+              logicalOperation: "or"
+            }
+          ],
+          logicalOperation: "or"
+        }]
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
--- src/lib/src/main.js
+++ src/lib/src/main.js
@@ -3,10 +3,103 @@ export default {
   props: {
     tree: {
       default: []
+    },
+    logicOperatorList:{
+      type: Array,
+    },
+    operatorList:{
+      type: Array,
+    },
+    propertyList:{
+      type: Array
     }
   },
   computed: {},
-  render(createElement){
-    return (<div><pre>{JSON.stringify(this.$props,null,2)}</pre></div>)
+  render(h){
+    let vm =this;
+    let tree = ( this.$props.tree instanceof Array) ? [...this.$props.tree] : [{...this.$props.tree}];
+
+    function solveItem(item) {
+      let options_arr=[];
+      for(let i=0;i<vm.propertyList.length;i++){
+        let _item = vm.propertyList[i];
+        options_arr.push(<option value="" selected={_item.value==item.propertyCode}>{_item.label}</option>)
+      }
+
+      let op_options_arr=[];
+      for(let i=0;i<vm.operatorList.length;i++){
+        let _item = vm.operatorList[i];
+        op_options_arr.push(<option value="" selected={_item.value==item.operatorList}>{_item.label}</option>)
+      }
+
+      return (<div>
+
+        <select>
+          {options_arr}
+        </select>
+        <select>
+          {op_options_arr}
+        </select>
+        <input type="text" value={item.value}/>
+      </div>)
+    }
+
+    function solveCondition(list) {
+      let dom_arr = [];
+      for (let i = 0; i < list.length; i++) {
+        let _item = list[i];
+        dom_arr.push(<div>
+          {solveItem(_item)}
+        </div>)
+      }
+      return dom_arr
+    };
+
+    function solveGroup(group) {
+      let dom_arr = [];
+      for (let k in group) {
+        if (k == "logicalOperation") {
+        }
+        else if (k == "groups") {
+          let __arr = [];
+          for (let i = 0; i < group[k].length; i++) {
+            __arr.push(solveGroup(group[k][i]))
+          }
+          dom_arr.push(__arr);
+        }
+        else if (k == 'conditions') {
+          dom_arr.push(solveCondition(group[k]))
+        }
+
+      }
+      let selectOption=[];
+      for(let i=0;i<vm.$props.logicOperatorList.length;i++){
+        let item = vm.$props.logicOperatorList[i];
+        selectOption.push(<option selected={item.value==group.logicalOperation}>{item.label}</option>)
+      }
+      return (<div>
+        <select>
+          {selectOption}
+          </select>
+        <div style="margin-left: 20px;">{dom_arr}</div>
+      </div>);
+    }
+
+    function solveList(list) {
+      let dom_arr = [];
+      for (let i = 0; i < list.length; i++) {
+        let item = tree[i];
+        if ('logicalOperation' in item) {
+          dom_arr.push(solveGroup(item))
+        }
+        else if (!('logicalOperation' in item) && ('operator' in item)) {
+          dom_arr.push(solveItem(item))
+        }
+      }
+      return (<div>{dom_arr}</div>);
+    }
+
+
+    return (<div>{solveList(tree)}</div>)
   }
 };
